92ac4a1b2e2807b817e33e00e1479dbe9cc69962
H3X-C0DE <vetleT.kodehode@gmail.com>
2023-12-06 12:06:42 +0100

    Game Works!!

--- C-Snake/GameState.cs
+++ C-Snake/GameState.cs
@@ -15,6 +15,8 @@ namespace C_Snake
         public Direction Dir { get; private set; }
         public int Score { get; private set; }
         public bool GameOver { get; private set; }
+
+        private readonly LinkedList<Direction> dirChanges = new LinkedList<Direction>();
         private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
         private readonly Random random = new Random();
         public GameState(int rows, int cols)
@@ -83,12 +85,35 @@ namespace C_Snake
         {
             Position tail = snakePositions.Last.Value;
             Grid[tail.Row, tail.Col] = GridValue.Empty;
-            snakePositions.RemoveFirst();
+            snakePositions.RemoveLast();
+        }
+
+        private Direction GetLastDirection()
+        {
+            if (dirChanges.Count == 0)
+            {
+                return Dir;
+            }
+            return dirChanges.Last.Value;
+        }
+
+        private bool CanChangeDirection(Direction newDir)
+        {
+            if (dirChanges.Count == 2)
+            {  
+                return false;
+            }
+
+            Direction lastDir = GetLastDirection();
+            return newDir != lastDir && newDir != lastDir.Opposite();
         }
 
         public void ChangeDirection(Direction dir)
         {
-            Dir = dir;
+            if (CanChangeDirection(dir))
+            {
+                dirChanges.AddLast(dir);
+            }
         }
 
         private bool OutsideGrid(Position pos)
@@ -113,6 +138,13 @@ namespace C_Snake
 
         public void Move()
         {
+            if (dirChanges.Count > 0)
+            {
+                Dir = dirChanges.First.Value;
+                dirChanges.RemoveFirst();
+        
+            }
+
             Position newHeadPos = HeadPosition().Translate(Dir);
             GridValue hit = WillHit(newHeadPos);
             if (hit == GridValue.Outside || hit == GridValue.Snake) 
